1bb369bf4857ea947fa011b133ec589aeec08641
Nicolas LENOIR <nicolas.lenoir87@gmail.com>
2026-02-09 16:39:59 +0100

    refactor(share_cards): remove unused imports from various files to streamline code and improve readability.

--- lib/share_cards/presentation/page/home_share_cards.dart
+++ lib/share_cards/presentation/page/home_share_cards.dart
@@ -69,9 +69,6 @@ class _ShareCardsPageState extends ConsumerState<ShareCardsPage> {
 
   @override
   Widget build(BuildContext context) {
-    final AsyncValue<List<ShareCards>> state = ref.watch(
-      shareCardsControllerProvider,
-    );
 
     // Écouter les changements d'index pour recharger les données
     ref.listen<int>(indexProvider, (previous, next) {
--- lib/ui/templates/template_loan_list.dart
+++ lib/ui/templates/template_loan_list.dart
@@ -1,6 +1,5 @@
 import 'package:deck_share/share_cards/domain/loan_list_filter.dart';
 import 'package:deck_share/share_cards/domain/share_cards_model.dart';
-import 'package:deck_share/share_cards/presentation/controller/share_cards_controller.dart';
 import 'package:deck_share/share_cards/presentation/page/loan_details_page.dart';
 import 'package:deck_share/share_cards/presentation/providers/share_cards_providers.dart';
 import 'package:deck_share/ui/atom/atom_image.dart';
